5ab41be88a88a182e3de204ef467758d71bbab25
Alan Pazetto <alan.pazetto@vermont.com.br>
2018-08-17 17:00:28 -0300

    Started Ex 01

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -19,7 +19,47 @@ def main(script):
 
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+    preg = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(preg)
+
+    # case_id = 10229
+    # indices = preg_map[case_id]
+
+    # print(indices)
+    # print( preg.outcome[indices].values )
+
+    # preg.loc[:13, 'prglngth'] = 1
+    # preg.loc[14:26, 'prglngth'] = 2
+    # preg.loc[26:, 'prglngth'] = 3
+
+
+
+
+    # df['totalwgt_kg'] = 
+
+
+
+
+
+    # ======================================================
+    # preg.loc[preg.prglngth.astype(int) <= 13, 'prglngth'] = 1
+    # preg.loc[ (preg.prglngth.astype(int) > 13) & (preg.prglngth.astype(int) <= 26), 'prglngth'] = 2
+    # preg.loc[preg.prglngth.astype(int) > 26, 'prglngth'] = 3
+
+    # preg.loc[preg.prglngth.astype(str) == '1', 'prglngth'] = '0-13'
+    # preg.loc[preg.prglngth.astype(str) == '2', 'prglngth'] = '13-26'
+    # preg.loc[preg.prglngth.astype(str) == '3', 'prglngth'] = '26-50'
+
+    # print( preg['prglngth'].value_counts().sort_index() )
+    # ======================================================
+
+    # print(birthord[0:10])
+
+
+    # print(preg.outcome.head())
+    # print(preg['pregordr'][:10])
+    # print( preg.outcome.value_counts(sort=False) )
+    # print('%s: All tests passed.' % script)
 
 
 if __name__ == '__main__':
